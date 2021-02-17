73c2b5fbdb23a117b38f204cc0ec3eb51f39943d
leagr <leagr@LAPTOP-AVJKEBSL.localdomain>
2021-02-17 10:48:57 +0100

    add the first version of the class Game

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,5 +1,30 @@
 package game;
 
 public class Game {
-    
+    private List<Player> players;
+    private Board board;
+
+    public Game(List<Player> players){
+        this.players= players;
+    }
+
+//savoir ou mettre n
+    public void play(){
+        int i =0;
+        boolean stop= false;
+        while(i<n && ! stop){
+            for(player:this.players){
+                player.action(this.board);
+                player.recolte();
+                player.feed();
+            }
+            if (this.board.allIsOccupated()){
+                stop=true;
+            }
+            i++;
+            player.daypass();
+            //pas forcement utile si on gere deja dans feed()
+        }
+        //ici calcul des points
+    }
 }
