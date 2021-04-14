b4d99a210220686941715aeac3305e6898dd39cd
herbin <clement.herbin.etu@univ-lille.fr>
2021-04-14 11:34:15 +0200

    fix: variable i not define in test

--- test/game/board/BoardTest.java
+++ test/game/board/BoardTest.java
@@ -78,7 +78,9 @@ public class BoardTest {
         int tab[][] = {{1, 2}, {3, 4}};
         int values[] = this.board.getValues(2, 2,tab, 4);
         int values2[] = {1, 2, 3, 4};
-        assertEquals(values[i], values2[i]);
+        for (int i = 0; i < values2.length; j++) {
+            assertEquals(values[i], values2[i]);
+        }
     }
 
 
