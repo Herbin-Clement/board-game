f9c10f5211d85ac965e2d5c0c3cbad43019b0511
herbin <clement.herbin.etu@univ-lille.fr>
2021-04-21 11:24:15 +0200

    fix: update display of a War Game

--- src/game/WarMain.java
+++ src/game/WarMain.java
@@ -10,10 +10,10 @@ public class WarMain{
     public static void main(String[] args) {
         WarBoard warboard = new WarBoard(20, 20);
         WarPlayer p1 = new WarPlayer("Alexis");
-        WarPlayer p2 = new WarPlayer("Lea");
+        // WarPlayer p2 = new WarPlayer("Lea");
         List<WarPlayer> wp = new ArrayList<WarPlayer>();
         wp.add(p1);
-        wp.add(p2);
+        // wp.add(p2);
         Game game = new WarGame(wp, warboard);
         game.play();
     }
